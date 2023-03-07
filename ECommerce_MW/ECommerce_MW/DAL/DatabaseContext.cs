05a094d51e0a52e75b7945d71ff64a6f5ca39b6f
Carlos Diaz <junior.diaz@selazar.com>
2023-03-07 05:57:23 -0500

    New ECommerce app - Creating DB with EF

---
+++ ECommerce_MW/ECommerce_MW/DAL/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using ECommerce_MW.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce_MW.DAL
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+    }
+}
---
+++ ECommerce_MW/ECommerce_MW/DAL/Entities/Country.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce_MW.DAL.Entities
+{
+    public class Country : Entity
+    {
+        [Display(Name = "País")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe ser de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es oblilgatorio.")]
+        public string Name { get; set; }
+    }
+}
