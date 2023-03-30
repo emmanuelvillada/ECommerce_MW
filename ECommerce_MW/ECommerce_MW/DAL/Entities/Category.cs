a1aba843dd0f8c001883eaed3246eef9a9918307
ASUS <ASUS@LAPTOP-10NQ3S9K>
2023-03-29 19:10:26 -0500

    New SeederDb to populate different tables and some code changes to improve th views

--- ECommerce_MW/ECommerce_MW/DAL/Entities/Category.cs
+++ ECommerce_MW/ECommerce_MW/DAL/Entities/Category.cs
@@ -8,5 +8,9 @@ namespace ECommerce_MW.DAL.Entities
         [MaxLength(50, ErrorMessage = "El campo {0} debe ser de {1} caracteres.")]
         [Required(ErrorMessage = "El campo {0} es oblilgatorio.")]
         public string Name { get; set; }
+
+        [Display(Name = "Descripción")]
+        [MaxLength(500, ErrorMessage = "El campo {0} debe ser de {1} caracteres.")]
+        public string? Description { get; set; }
     }
 }
