8b6b63bce34f6b8dcc3f5db8f99cc4b70a247519
ASUS <ASUS@LAPTOP-10NQ3S9K>
2023-05-03 18:48:30 -0500

    New CreateAdmin action in UserController.cs

--- ECommerce_MW/ECommerce_MW/Controllers/UsersController.cs
+++ ECommerce_MW/ECommerce_MW/Controllers/UsersController.cs
@@ -1,4 +1,8 @@
 ﻿using ECommerce_MW.DAL;
+using ECommerce_MW.DAL.Entities;
+using ECommerce_MW.Enums;
+using ECommerce_MW.Helpers;
+using ECommerce_MW.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -8,11 +12,17 @@ namespace ECommerce_MW.Controllers
     [Authorize(Roles = "Admin")]
     public class UsersController : Controller
     {
+        private readonly IUserHelper _userHelper;
         private readonly DatabaseContext _context;
+        private readonly IDropDownListsHelper _ddlHelper;
+        private readonly IAzureBlobHelper _azureBlobHelper;
 
-        public UsersController(DatabaseContext context)
+        public UsersController(IUserHelper userHelper, DatabaseContext context, IDropDownListsHelper dropDownListsHelper, IAzureBlobHelper azureBlobHelper)
         {
+            _userHelper = userHelper;
             _context = context;
+            _ddlHelper = dropDownListsHelper;
+            _azureBlobHelper = azureBlobHelper;
         }
 
         public async Task<IActionResult> Index()
@@ -24,6 +34,81 @@ namespace ECommerce_MW.Controllers
                 .ToListAsync());
         }
 
+        [HttpGet]
+        public async Task<IActionResult> CreateAdmin()
+        {
+            Guid emptyGuid = new Guid();
+
+            AddUserViewModel addUserViewModel = new()
+            {
+                Id = Guid.Empty,
+                Countries = await _ddlHelper.GetDDLCountriesAsync(),
+                States = await _ddlHelper.GetDDLStatesAsync(emptyGuid),
+                Cities = await _ddlHelper.GetDDLCitiesAsync(emptyGuid),
+                UserType = UserType.Admin,
+            };
+
+            return View(addUserViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateAdmin(AddUserViewModel addUserViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid imageId = Guid.Empty;
+
+                if (addUserViewModel.ImageFile != null)
+                    imageId = await _azureBlobHelper.UploadAzureBlobAsync(addUserViewModel.ImageFile, "users");
+
+                addUserViewModel.ImageId = imageId;
+                addUserViewModel.CreatedDate = DateTime.Now;
+
+                User user = await _userHelper.AddUserAsync(addUserViewModel);
+                if (user == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Este correo ya está siendo usado.");
+                    await FillDropDownListLocation(addUserViewModel);
+                    return View(addUserViewModel);
+                }
+
+                return RedirectToAction("Index", "Users");
+            }
+
+            await FillDropDownListLocation(addUserViewModel);
+            return View(addUserViewModel);
+        }
+
+        private async Task FillDropDownListLocation(AddUserViewModel addUserViewModel)
+        {
+            addUserViewModel.Countries = await _ddlHelper.GetDDLCountriesAsync();
+            addUserViewModel.States = await _ddlHelper.GetDDLStatesAsync(addUserViewModel.CountryId);
+            addUserViewModel.Cities = await _ddlHelper.GetDDLCitiesAsync(addUserViewModel.StateId);
+        }
+
+        [HttpGet]
+        public JsonResult GetStates(Guid countryId)
+        {
+            Country country = _context.Countries
+                .Include(c => c.States)
+                .FirstOrDefault(c => c.Id == countryId);
+
+            if (country == null) return null;
+
+            return Json(country.States.OrderBy(d => d.Name));
+        }
+
+        [HttpGet]
+        public JsonResult GetCities(Guid stateId)
+        {
+            State state = _context.States
+                .Include(s => s.Cities)
+                .FirstOrDefault(s => s.Id == stateId);
+            if (state == null) return null;
+
+            return Json(state.Cities.OrderBy(c => c.Name));
+        }
 
     }
 }
