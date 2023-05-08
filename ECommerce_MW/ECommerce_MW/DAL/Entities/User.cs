15c67aaff1914fde2c6229bfccc0ae96b2b043a2
ASUS <ASUS@LAPTOP-10NQ3S9K>
2023-05-08 14:27:42 -0500

    Enhance some const names

--- ECommerce_MW/ECommerce_MW/DAL/Entities/User.cs
+++ ECommerce_MW/ECommerce_MW/DAL/Entities/User.cs
@@ -13,17 +13,17 @@ namespace ECommerce_MW.DAL.Entities
         public DateTime? ModifiedDate { get; set; }
 
         [Display(Name = "Documento")]
-        [MaxLength(20, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [MaxLength(20, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Document { get; set; }
 
         [Display(Name = "Nombres")]
-        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string FirstName { get; set; }
 
         [Display(Name = "Apellidos")]
-        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string LastName { get; set; }
         
