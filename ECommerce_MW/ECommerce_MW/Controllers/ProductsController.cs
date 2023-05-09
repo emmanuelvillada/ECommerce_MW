8ba69b49531b760acb2b51221829071aff52c0ab
ASUS <ASUS@LAPTOP-10NQ3S9K>
2023-05-08 19:10:17 -0500

    ProductController created with Index action

---
+++ ECommerce_MW/ECommerce_MW/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using ECommerce_MW.DAL;
+using ECommerce_MW.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ECommerce_MW.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [AllowAnonymous]
+    public class ProductsController : Controller
+    {
+        private readonly DatabaseContext _context;
+        private readonly IDropDownListsHelper _dropDownListsHelper;
+        private readonly IAzureBlobHelper _azureBlobHelper;
+
+        public ProductsController(DatabaseContext context, IDropDownListsHelper dropDownListsHelper, IAzureBlobHelper azureBlobHelper)
+        {
+            _context = context;
+            _dropDownListsHelper = dropDownListsHelper;
+            _azureBlobHelper = azureBlobHelper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products
+                .Include(p => p.ProductImages)
+                .Include(p => p.ProductCategories)
+                .ThenInclude(pc => pc.Category)
+                .ToListAsync());
+        }
+    }
+}
