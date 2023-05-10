cc6d0300bfa96b15af71798533abec89577b2cc7
ASUS <ASUS@LAPTOP-10NQ3S9K>
2023-05-10 18:33:07 -0500

    Action Edit GET & POST created

--- ECommerce_MW/ECommerce_MW/Controllers/ProductsController.cs
+++ ECommerce_MW/ECommerce_MW/Controllers/ProductsController.cs
@@ -105,5 +105,56 @@ namespace ECommerce_MW.Controllers
             addProductViewModel.Categories = await _dropDownListsHelper.GetDDLCategoriesAsync();
             return View(addProductViewModel);
         }
+
+        public async Task<IActionResult> Edit(Guid? productId)
+        {
+            if (productId == null) return NotFound();
+
+            Product product = await _context.Products.FindAsync(productId);
+            if (product == null) return NotFound();
+
+            EditProductViewModel editProductViewModel = new()
+            {
+                Description = product.Description,
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                Stock = product.Stock,
+            };
+
+            return View(editProductViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid? Id, EditProductViewModel editProductViewModel)
+        {
+            if (Id != editProductViewModel.Id) return NotFound();
+
+            try
+            {
+                Product product = await _context.Products.FindAsync(editProductViewModel.Id);
+                product.Description = editProductViewModel.Description;
+                product.Name = editProductViewModel.Name;
+                product.Price = editProductViewModel.Price;
+                product.Stock = editProductViewModel.Stock;
+                _context.Update(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    ModelState.AddModelError(string.Empty, "Ya existe un producto con el mismo nombre.");
+                else
+                    ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+            }
+            catch (Exception exception)
+            {
+                ModelState.AddModelError(string.Empty, exception.Message);
+            }
+
+            return View(editProductViewModel);
+        }
     }
 }
