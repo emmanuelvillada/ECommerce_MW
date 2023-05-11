a8e93c12baf26c59e5a0ce9f2a5c7d6e4238f3b8
ASUS <ASUS@LAPTOP-10NQ3S9K>
2023-05-10 19:02:40 -0500

    DeleteImage action created

--- ECommerce_MW/ECommerce_MW/Controllers/ProductsController.cs
+++ ECommerce_MW/ECommerce_MW/Controllers/ProductsController.cs
@@ -217,6 +217,21 @@ namespace ECommerce_MW.Controllers
             return View(addProductImageViewModel);
         }
 
+        public async Task<IActionResult> DeleteImage(Guid? imageId)
+        {
+            if (imageId == null) return NotFound();
+
+            ProductImage productImage = await _context.ProductImages
+                .Include(pi => pi.Product)
+                .FirstOrDefaultAsync(pi => pi.Id == imageId);
+            
+            if (productImage == null) return NotFound();
+
+            await _azureBlobHelper.DeleteAzureBlobAsync(productImage.ImageId, "products");
+            _context.ProductImages.Remove(productImage);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Details), new { productId = productImage.Product.Id });
+        }
 
     }
 }
